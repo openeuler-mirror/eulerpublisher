cb82186b426a1e5d0b64d1f246718f7fcd6773cf
hxc1017 <1635728840@qq.com>
2024-05-12 21:51:43 +0800

    添加go的测试用例

---
+++ tests/container/app/go_test.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+. "$(dirname "$0")/../common/common_funs.sh"
+. "$(dirname "$0")/../common/common_vars.sh"
+
+# cheat sheet:
+#  assertTrue $?
+#  assertEquals ["explanation"] 1 2
+#  oneTimeSetUp()
+#  oneTimeTearDown()
+#  setUp() - run before each test
+#  tearDown() - run after each test
+
+oneTimeSetUp() {
+    # Remove image before test.
+    remove_current_image
+
+    # Make sure we're using the latest OCI image.
+    debug "pulling image ${DOCKER_IMAGE}"
+    docker pull --quiet "${DOCKER_IMAGE}" > /dev/null
+    debug "done pulling image ${DOCKER_IMAGE}"
+
+    docker network create "$DOCKER_NETWORK" > /dev/null 2>&1
+}
+
+oneTimeTearDown() {
+    docker network rm "$DOCKER_NETWORK" > /dev/null 2>&1
+}
+
+# 运行go容器
+docker_run_go(){
+  docker run \
+    --rm \
+    --name go_test \
+    "${DOCKER_IMAGE}" \
+    go version
+}
+
+# 测试go容器运行是否成功
+test_go_start() {
+    debug "Creating go container"
+
+    out=$(docker_run_go)
+    assertNotNull "Failed to start the container" "${out}" || return 1
+
+    expected="go version go1.17.3 linux/amd64"
+    assertEquals "Unexpected go version" "${expected}" "${out}" || return 1
+}
+
+# Load shUnit2.
+load_shunit2
