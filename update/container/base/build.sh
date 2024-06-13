5928fdbcab057312e88e546c8c39acf3c16eea3e
GuangJie1 <guangjiebai@gmail.com>
2024-06-13 15:12:51 +0800

    pushrm: add pushrm script for auto publish image readme

--- update/container/base/build.sh
+++ update/container/base/build.sh
@@ -11,6 +11,8 @@ echo -e "\n[STAGE] Install eulerpublisher"
 if [[which eulerpublisher > /dev/null 2>&1]]; then
     sudo pip3 uninstall -y eulerpublisher
 fi
+
+rm -rf eulerpublisher
 git clone https://gitee.com/openeuler/eulerpublisher.git
 cd eulerpublisher
 pip3 install -r ./requirement.txt
