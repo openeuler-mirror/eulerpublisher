b152f43eb299f40411f88d356c9f13e755a1323e
GuangJie1 <guangjiebai@gmail.com>
2024-06-13 15:34:57 +0800

    pushrm: add pushrm script for auto publish image readme

--- update/container/app/build.sh
+++ update/container/app/build.sh
@@ -1,23 +1,28 @@
-#!/bin/bash
+#!/bin/sh
 
+if [ "$state" != "merged" ]; then
+    exit 0
+fi
+# start docker
 if [ docker info > /dev/null 2>&1 ]; then
     systemctl start docker
     echo "starting docker..."
 fi
-
+# install newest eulerpublisher
 if [ which eulerpublisher > /dev/null 2>&1 ]; then
-    sudo pip3 uninstall -y eulerpublisher
+    pip3 uninstall -y eulerpublisher
 fi
-
 rm -rf eulerpublisher/
 git clone https://gitee.com/openeuler/eulerpublisher.git
 cd eulerpublisher
 pip3 install -r ./requirement.txt
 python3 setup.py install
-
+# publish to hubs
 sudo -E python3 update/container/app/update.py \
-	-pr ${prid} \
-    -sr ${repo} \
-    -su ${scodeurl} \
-    -br ${branch} \
-    -op check
+	-pr ${giteePullRequestIid} \
+    -sr ${giteeRepoName} \
+    -su ${giteeTargetRepoUrl} \
+    -br ${giteeTargetBranch} \
+    -op push
+# clean cache
+rm -rf /tmp/eulerpublisher
