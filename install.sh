61b7ecc1f96fe37ace472f514d8c5b2dd442aa5a
zhihang <zhihang161013@outlook.com>
2024-10-11 07:29:38 +0000

    fix: modify install.sh and uninstall.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,15 @@
 
 set -e
 
+if [[ "$(uname -m)" == "x86_64" ]]; then
+    ARCH="x86_64"
+elif [[ "$(uname -m)" == "aarch64" ]]; then
+    ARCH="aarch64"
+else
+    echo "Unrecognized arch $(uname -m)"
+    exit 1
+fi
+
 # 安装eulerpublisher
 sudo python3 setup.py install --record install_log.txt
 
@@ -15,76 +24,65 @@ sudo yum install -y qemu-img
 mkdir -p utils
 
 # 安装docker
-if [[ ! -e "utils/docker.tar.gz" ]]; then
-    if [[ "$(uname -m)" == "aarch64" ]]; then
-        wget https://download.docker.com/linux/static/stable/aarch64/docker-24.0.9.tgz -O utils/docker.tar.gz
-    elif [[ "$(uname -m)" == "x86_64" ]]; then
-        wget https://download.docker.com/linux/static/stable/x86_64/docker-24.0.9.tgz -O utils/docker.tar.gz
-    else
-        echo "Unrecognized arch $(uname -m)"
-        exit 1
+if [[ ! $(which docker) ]]; then
+    wget https://download.docker.com/linux/static/stable/${ARCH}/docker-24.0.9.tgz -O utils/docker.tar.gz
+    mkdir -p utils/docker
+    tar -zxf utils/docker.tar.gz -C utils/docker --strip-components=1
+    sudo cp -p utils/docker/* /usr/local/bin
+    if [[ ! $(sudo grep -q docker /etc/group) ]]; then
+        sudo groupadd docker || true
     fi
-fi
-mkdir -p utils/docker
-tar -zxf utils/docker.tar.gz -C utils/docker --strip-components=1
-sudo cp -p utils/docker/* /usr/local/bin
-if ! sudo grep -q docker /etc/group; then
-    sudo groupadd docker || true
-fi
-if [[ ! -z ${USER:-} ]]; then
-    sudo usermod -aG docker $USER || true
-else
-    sudo usermod -aG docker openeuler || true
+    if [[ ! -z ${USER:-} ]]; then
+        sudo usermod -aG docker $USER || true
+    else
+        sudo usermod -aG docker openeuler || true
+    fi
 fi
 
 # 安装slim
-curl -sL https://raw.githubusercontent.com/slimtoolkit/slim/master/scripts/install-slim.sh | sudo -E bash -
+if [[ ! $(which slim) ]]; then
+    curl -sL https://raw.githubusercontent.com/slimtoolkit/slim/master/scripts/install-slim.sh | sudo -E bash -
+fi
 
-# 安装云厂商命令行工具
-if [[ "$(uname -m)" == "aarch64" ]]; then
-    if [[ ! -e "utils/awsutil.zip" ]]; then
-        wget "https://awscli.amazonaws.com/awscli-exe-linux-aarch64.zip" -O utils/awsutil.zip
-    fi
-    if [[ ! -e "utils/obsutil.tar.gz" ]]; then
-        wget "https://obs-community.obs.cn-north-1.myhuaweicloud.com/obsutil/current/obsutil_linux_arm64.tar.gz" -O utils/obsutil.tar.gz
-    fi
-    if [[ ! -e "utils/coscli" ]]; then
-        wget "https://cosbrowser.cloud.tencent.com/software/coscli/coscli-v1.0.1-linux-arm64" -O utils/coscli
-    fi
-    if [[ ! -e "utils/ossutil.zip" ]]; then
-        wget "https://gosspublic.alicdn.com/ossutil/1.7.19/ossutil-v1.7.19-linux-arm64.zip" -O utils/ossutil.zip
-    fi
-elif [[ "$(uname -m)" == "x86_64" ]]; then
-    if [[ ! -e "utils/awsutil.zip" ]]; then
-        wget "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -O utils/awsutil.zip
-    fi 
-    if [[ ! -e "utils/obsutil.tar.gz" ]]; then
-        wget "https://obs-community.obs.cn-north-1.myhuaweicloud.com/obsutil/current/obsutil_linux_amd64.tar.gz" -O utils/obsutil.tar.gz
-    fi
-    if [[ ! -e "utils/coscli" ]]; then
-        wget "https://cosbrowser.cloud.tencent.com/software/coscli/coscli-v1.0.1-linux-amd64" -O utils/coscli
-    fi
-    if [[ ! -e "utils/ossutil.zip" ]]; then
-        wget "https://gosspublic.alicdn.com/ossutil/1.7.19/ossutil-v1.7.19-linux-amd64.zip" -O utils/ossutil.zip
-    fi
+# 安装公有云厂商命令行工具
+if [[ ! $(which aws) ]]; then
+    wget https://awscli.amazonaws.com/awscli-exe-linux-${ARCH}.zip -O utils/awsutil.zip
+    unzip utils/awsutil.zip -d utils
+    sudo bash utils/aws/install
+fi
+
+if [[ "$(uname -m)" == "x86_64" ]]; then
+    ARCH="amd64"
+elif [[ "$(uname -m)" == "aarch64" ]]; then
+    ARCH="arm64"
 else
     echo "Unrecognized arch $(uname -m)"
     exit 1
 fi
-unzip utils/awsutil.zip -d utils
-sudo bash utils/aws/install
-mkdir -p utils/obsutil
-tar -xvf utils/obsutil.tar.gz -C utils/obsutil --strip-components=1
-sudo cp -p utils/obsutil/obsutil /usr/local/bin
-chmod 755 utils/coscli
-sudo cp -p utils/coscli /usr/local/bin
-mkdir -p utils/ossutil
-unzip -j utils/ossutil.zip -d utils/ossutil
-sudo cp -p utils/ossutil/ossutil /usr/local/bin
+
+if [[ ! $(which obsutil) ]]; then
+    wget https://obs-community.obs.cn-north-1.myhuaweicloud.com/obsutil/current/obsutil_linux_${ARCH}.tar.gz -O utils/obsutil.tar.gz
+    mkdir -p utils/obsutil
+    tar -xvf utils/obsutil.tar.gz -C utils/obsutil --strip-components=1
+    sudo cp -p utils/obsutil/obsutil /usr/local/bin
+fi
+
+if [[ ! $(which coscli) ]]; then
+    wget https://cosbrowser.cloud.tencent.com/software/coscli/coscli-v1.0.1-linux-${ARCH} -O utils/coscli
+    chmod 755 utils/coscli
+    sudo cp -p utils/coscli /usr/local/bin
+fi
+
+if [[ ! $(which ossutil) ]]; then
+    wget https://gosspublic.alicdn.com/ossutil/1.7.19/ossutil-v1.7.19-linux-${ARCH}.zip -O utils/ossutil.zip
+    mkdir -p utils/ossutil
+    unzip -j utils/ossutil.zip -d utils/ossutil
+    sudo cp -p utils/ossutil/ossutil /usr/local/bin
+fi
 
 # 安装shUnit2
-if [[ ! -e "utils/shunit2.tar.gz" ]]; then
+if [[ ! -e "/usr/share/shunit2" ]]; then
     wget https://github.com/kward/shunit2/archive/refs/tags/v2.1.8.tar.gz -O utils/shunit2.tar.gz
+    mkdir -p /usr/share/shunit2
+    tar -xvf utils/shunit2.tar.gz -C /usr/share/shunit2 --strip-components=1
 fi
-mkdir -p /usr/share/shunit2
-tar -xvf utils/shunit2.tar.gz -C /usr/share/shunit2 --strip-components=1
--- uninstall.sh
+++ uninstall.sh
@@ -5,7 +5,7 @@ set -e
 # 卸载eulerpublisher 
 cat install_log.txt | sudo xargs rm -rf
 rm -rf install_log.txt
-rm -rf build eulerpublisher.egg-info
+rm -rf build eulerpublisher.egg-info .egg
 
 # 卸载python依赖 
 sudo pip uninstall -yr requirements.txt
@@ -14,24 +14,29 @@ sudo pip uninstall -yr requirements.txt
 sudo yum autoremove -y qemu-img
 
 # 卸载docker
-for bin in $(ls utils/docker)
-do
-    sudo rm -rf /usr/local/bin/$bin
-done
+sudo rm -rf /usr/local/bin/containerd   \
+            /usr/local/bin/ctr          \
+            /usr/local/bin/dockerd      \
+            /usr/local/bin/docker-proxy \
+            /usr/local/bin/docker       \
+            /usr/local/bin/docker-init  \
+            /usr/local/bin/runc         \
+            /usr/local/bin/containerd-shim-runc-v2  
 
 # 卸载云厂商命令行工具
-rm -rf /usr/local/bin/coscli  \
-       /usr/local/bin/obsutil \
-       /usr/local/bin/ossutil \
-       /usr/local/aws-cli     \
-       /usr/local/bin/aws     \
-       /usr/local/bin/aws_completer
+sudo rm -rf /usr/local/bin/coscli  \
+            /usr/local/bin/obsutil \
+            /usr/local/bin/ossutil \
+            /usr/local/aws-cli     \
+            /usr/local/bin/aws     \
+            /usr/local/bin/aws_completer
 
 # 卸载slim
-sudo rm -rf /usr/local/bin/slim /usr/local/bin/slim-sensor
+sudo rm -rf /usr/local/bin/slim \
+            /usr/local/bin/slim-sensor
 
 # 卸载shUnit2
 sudo rm -rf /usr/share/shunit2
 
 # 清理工具集
-rm -rf utils/*
+rm -rf utils
