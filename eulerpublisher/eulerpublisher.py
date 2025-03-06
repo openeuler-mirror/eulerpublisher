08e55b395a2690460f2e89f48470eba60b77643b
junlong1 <zhangjunlongtech@foxmail.com>
2025-03-06 09:17:47 +0000

    Initialization frame

--- eulerpublisher/eulerpublisher.py
+++ eulerpublisher/eulerpublisher.py
@@ -1,21 +1,13 @@
 # coding=utf-8
 import click
 
-from eulerpublisher.container import cli
-
 
 @click.group()
 def entrance():
     pass
 
 
-def _add_commands():
-    # Unified interface for extension.
-    entrance.add_command(cli.group)
-
-
 def main():
-    _add_commands()
     entrance()
 
 
