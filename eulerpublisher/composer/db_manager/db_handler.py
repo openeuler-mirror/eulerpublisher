82b8c300b7315b97930b8964d75c0df9d0138b18
Icey <1790571317@qq.com>
2025-03-17 11:10:16 +0800

    Add recursive incremental building

--- eulerpublisher/composer/db_manager/db_handler.py
+++ eulerpublisher/composer/db_manager/db_handler.py
@@ -28,17 +28,37 @@ class DBHandler:
             logging.error(f"Failed to add software {software_name}: {e}")
             raise
 
-    def add_version(self, software_name, version):
+    def get_software_name_by_software_id(self, software_id):
+        try:
+            with self.get_db_connection() as conn:
+                cursor = conn.cursor()
+                cursor.execute('SELECT software_name FROM software_data WHERE software_id = ?', (software_id,))
+                software_name_result = cursor.fetchone()
+                if not software_name_result:
+                    raise ValueError(f"Software {software_id} does not exist in the database.")
+                return software_name_result[0]
+        except sqlite3.Error as e:
+            logging.error(f"Failed to get software name for software {software_id}: {e}")
+            raise
+        
+    def get_software_id_by_software_name(self, software_name):
         try:
             with self.get_db_connection() as conn:
                 cursor = conn.cursor()
                 cursor.execute('SELECT id FROM software_data WHERE software_name = ?', (software_name,))
-                result = cursor.fetchone()
-                if not result:
+                software_id_result = cursor.fetchone()
+                if not software_id_result:
                     raise ValueError(f"Software {software_name} does not exist in the database.")
-                software_id = result[0]
-                self.execute_query('INSERT INTO version_data (software_id, version) VALUES (?, ?)', 
-                                   (software_id, version))
+                return software_id_result[0]
+        except sqlite3.Error as e:
+            logging.error(f"Failed to get software id for software {software_name}: {e}")
+            raise
+
+    def add_version(self, software_name, version):
+        try:
+            software_id = self.get_software_id_by_software_name(software_name)
+            self.execute_query('INSERT INTO version_data (software_id, version) VALUES (?, ?)', 
+                                (software_id, version))
         except sqlite3.Error as e:
             logging.error(f"Failed to add version {version} for software {software_name}: {e}")
             raise
@@ -53,20 +73,28 @@ class DBHandler:
         except sqlite3.Error as e:
             logging.error(f"Failed to fetch software names: {e}")
             raise
-        
+            
     def get_versions_by_software_name(self, software_name):
         try:
             with self.get_db_connection() as conn:
                 cursor = conn.cursor()
-                cursor.execute('SELECT id FROM software_data WHERE software_name = ?', (software_name,))
-                software_id_result = cursor.fetchone()
-                if not software_id_result:
-                    raise ValueError(f"Software {software_name} does not exist in the database.")
-                software_id = software_id_result[0]
+                software_id = self.get_software_id_by_software_name(software_name)
                 cursor.execute('SELECT version FROM version_data WHERE software_id = ?', (software_id,))
                 results = cursor.fetchall()
-            return [result[0] for result in results]
+                return [result[0] for result in results]
         except sqlite3.Error as e:
             logging.error(f"Failed to fetch versions for software {software_name}: {e}")
             raise
         
+    def get_depend_software_id_by_software_name(self, software_name):
+        try:
+            with self.get_db_connection() as conn:
+                cursor = conn.cursor()
+                software_id = self.get_software_id_by_software_name(software_name)
+                cursor.execute('SELECT depend_software_id FROM dependency_data WHERE software_id = ?', (software_id,))
+                results = cursor.fetchall()
+                return [result[0] for result in results]
+        except sqlite3.Error as e:
+            logging.error(f"Failed to fetch versions for software {software_name}: {e}")
+            raise
+
--- eulerpublisher/composer/version_composer/version_monitor.py
+++ eulerpublisher/composer/version_composer/version_monitor.py
@@ -1,8 +1,6 @@
 import requests
 import logging
 
-from version_combinator import VersionCombinator
-from eulerpublisher.composer.db_manager.db_handler import DBHandler
 from eulerpublisher.utils.constants import BASE_URL
 
 class APIMonitor:
@@ -25,7 +23,7 @@ class APIMonitor:
     def filter_python_versions(self, versions):
         return [version for version in versions if version.count('.') == 1]
     
-    def get_api_latest_versions(self,software_name, versions):
+    def get_api_latest_versions(self, software_name, versions):
         if software_name == "python":
             filtered_versions = self.filter_python_versions(versions)
             return filtered_versions[:2]
@@ -38,21 +36,3 @@ class APIMonitor:
             return filtered_versions[0] if filtered_versions else None
         else:
             return versions[0] if versions else None
-                
-    def schedule_task(self):
-        software_names = DBHandler.get_all_software_names()
-        for software_name in software_names:
-            old_versions = DBHandler.get_versions_by_software_name(software_name)
-            cur_versions = self.fetch_software_data(software_name)
-            if not cur_versions:
-                logging.warning(f"No versions found for software: {software_name}")
-                continue
-            latest_two_versions = self.get_api_latest_versions(software_name, cur_versions)
-                
-            if not old_versions:
-                for version in latest_two_versions:
-                    VersionCombinator.combine_version(software_name, version)
-            else:
-                api_first_version = self.get_api_first_version(software_name, cur_versions)
-                if api_first_version and api_first_version not in old_versions:
-                    VersionCombinator.combine_version(software_name, api_first_version)
