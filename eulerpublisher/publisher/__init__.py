af37708c4881a76ec9e1f655ec8dec0d99121ff2
GuangJie1 <guangjiebai@gmail.com>
2025-04-01 11:33:52 +0800

    fix: base dockerfile path

--- eulerpublisher/publisher/__init__.py
+++ eulerpublisher/publisher/__init__.py
@@ -4,7 +4,7 @@ import os
 
 EP_PATH = None
 OPENEULER_REPO = "http://repo.openeuler.org/"
-OPENEULER_DOCKERFILE = "https://gitee.com/openeuler/openeuler-docker-images/raw/master/openeuler/Dockerfile"
+OPENEULER_DOCKERFILE = "https://gitee.com/openeuler/openeuler-docker-images/raw/master/Base/openeuler/Dockerfile"
 
 paths = [
     os.path.dirname(os.__file__) + "/" + "../../etc/eulerpublisher/",
