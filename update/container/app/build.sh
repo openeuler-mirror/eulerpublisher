26d57dbb7bc61f3dd66f3e8a687d0ed7eb7332a0
sunshuang1866 <sunshuang1866@outlook.com>
2026-02-09 08:56:44 +0000

    fix CI failure
    Signed-off-by: sunshuang1866 <sunshuang1866@outlook.com>

--- update/container/app/build.sh
+++ update/container/app/build.sh
@@ -10,12 +10,12 @@ docker volume rm $(docker volume ls -q) --force > /dev/null 2>&1 || true
 docker system df
 echo "清理完成!"
 
-if ! docker info > /dev/null 2>&1; then
+if [ docker info > /dev/null 2>&1 ]; then
     systemctl start docker
     echo "starting docker..."
 fi
 
-if command -v eulerpublisher > /dev/null 2>&1; then
+if [ which eulerpublisher > /dev/null 2>&1 ]; then
     sudo pip3 uninstall -y eulerpublisher > /dev/null 2>&1
 fi
 
@@ -29,8 +29,10 @@ cd ../
 rm -rf eulerpublisher/
 git clone https://gitcode.com/openeuler/eulerpublisher.git
 cd eulerpublisher
+pip3 uninstall -y eulerpublisher > /dev/null 2>&1 || true
+rm -rf /usr/local/lib/python3.*/site-packages/eulerpublisher*
 pip3 install -r ./requirements.txt > /dev/null 2>&1
-python3 setup.py install > /dev/null 2>&1
+pip3 install . > /dev/null 2>&1
 
 sudo -E python3 update/container/app/update.py \
 	-pr ${prid} \
